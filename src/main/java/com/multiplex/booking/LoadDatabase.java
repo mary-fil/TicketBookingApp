09ad41706ccf58ce1d8969a14d2197caffd1fb74
Maria Filocha <maritka10@gmail.com>
2023-06-05 09:13:00 +0200

    Not using LoadDatabase

--- src/main/java/com/multiplex/booking/LoadDatabase.java
+++ src/main/java/com/multiplex/booking/LoadDatabase.java
@@ -1,3 +1,7 @@
+// This class was previously used to load database.
+// Leaving here if needed in future.
+
+/*
 package com.multiplex.booking;
 
 import org.slf4j.Logger;
@@ -13,29 +17,30 @@ class LoadDatabase {
 
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-  // Code previously used to load database:
+  Room room1 = new Room(1,1,5);
+  Room room2 = new Room(2,2,10);
+  Room room3 = new Room(3,2,10);
 
-  // Room room1 = new Room(1,1,5);
-  // Room room2 = new Room(2,2,10);
-  // Room room3 = new Room(3,2,10);
-
-  // @Bean
-  // CommandLineRunner initDatabaseRoom(RoomRepository repository) {
+  @Bean
+  CommandLineRunner initDatabaseRoom(RoomRepository repository) {
     
-  //   return args -> {
-  //     log.info("Preloading " + repository.save(room1));
-  //     log.info("Preloading " + repository.save(room2));
-  //     log.info("Preloading " + repository.save(room3));
-  //   };
-  // }
+    return args -> {
+      //log.info("Preloading " + repository.save(room1));
+      //log.info("Preloading " + repository.save(room2));
+      //log.info("Preloading " + repository.save(room3));
+    };
+  }
 
-  // @Bean
-  // CommandLineRunner initDatabaseShowing(ShowingRepository repository) {
+  @Bean
+  CommandLineRunner initDatabaseShowing(ShowingRepository repository) {
 
-  //   return args -> {
-  //     log.info("Preloading " + repository.save(new Showing(LocalDateTime.of(2023, 7, 24, 20, 0), "b", room1)));
-  //     log.info("Preloading " + repository.save(new Showing(LocalDateTime.of(2023, 6, 24, 18, 0), "a", room2)));
-  //     log.info("Preloading " + repository.save(new Showing(LocalDateTime.of(2023, 5, 31, 21, 0), "a", room3)));
-  //   };
-  // }
+    return args -> {
+      //log.info("Preloading " + repository.save(new Showing(LocalDateTime.of(2023, 7, 24, 20, 0), "b", room1)));
+      //log.info("Preloading " + repository.save(new Showing(LocalDateTime.of(2023, 6, 24, 18, 0), "a", room2)));
+      //log.info("Preloading " + repository.save(new Showing(LocalDateTime.of(2023, 5, 31, 21, 0), "a", room3)));
+    };
+  }
+
 }
+
+ */
